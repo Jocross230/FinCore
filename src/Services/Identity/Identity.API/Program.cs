f9dbf9304dd3e06ff086e794e317da428b6ca7a9
Onyeje Josiah <jocrossonyeje@gmail.com>
2026-08-11 19:38:27 +0100

    Remove empty line in Program.cs

--- src/Services/Identity/Identity.API/Program.cs
+++ src/Services/Identity/Identity.API/Program.cs
@@ -4,7 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
